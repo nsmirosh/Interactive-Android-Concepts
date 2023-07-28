9e04dc3b891d323a66623fd5b598ceaac4359bde
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-07-28 19:35:56 +0700

    Workign on parallax screen

--- app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
@@ -1,75 +1,139 @@
 package nick.mirosh.androidsamples.ui.parallax
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.ContentScale
-import coil.compose.AsyncImage
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import nick.mirosh.androidsamples.R
+
+
+private const val TAG = "ParallaxScreen"
 
 @Composable
 fun ParallaxScreen() {
-//    CollapsibleHeaderScreen()
-    CharacterScreen()
+    ScrollableCard()
 }
 
-
 @Composable
-fun CharacterScreen() {
-    val scrollState = rememberScrollState()
+fun ScrollableCard() {
+    val columnScrollState = rememberScrollState()
     Column(
         modifier = Modifier
-            .verticalScroll(scrollState)
+            .fillMaxWidth()
+            .verticalScroll(columnScrollState),
     ) {
-        val imageAddress = R.drawable.bangkok
-        Box {
-            AsyncImage(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .graphicsLayer {
-                        alpha =
-                            1f - ((scrollState.value.toFloat() / scrollState.maxValue) * 1.5f)
-                        translationY = 0.5f * scrollState.value
-                    },
-                contentDescription = "Character",
-                model = imageAddress,
-                contentScale = ContentScale.FillBounds,
-            )
-        }
-        Text(modifier = Modifier.background(Color.White), text = someRandomText)
 
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
+        ScrollableItem(
+            columnScrollState,
+            "Massimiliano Morosinotto",
+            R.drawable.picture1
+        )
     }
 }
 
 
 @Composable
-fun Counter() {
-    var count by remember { mutableIntStateOf(0) }
-
-    MaterialTheme {
-        Column {
-            Text(text = "You've clicked the button $count times")
-            Button(onClick = { count++ }) {
-                Text(text = "Click me")
-            }
+fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
+    var prevScrollValue by remember { mutableIntStateOf(0) }
+    Card(
+        modifier = Modifier.height(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = "Your Image",
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .verticalScroll(columnScrollState)
+                    .graphicsLayer {
+                        val yMovement =
+                            columnScrollState.value - prevScrollValue
+                        prevScrollValue = columnScrollState.value
+                        val adjusted = 0.5f * yMovement.toFloat()
+                        Log.d(TAG, "ScrollableCard: $adjusted")
+                        translationY = adjusted
+                    },
+            )
+            Text(
+                text = author,
+                modifier = Modifier.align(Alignment.BottomStart).padding(16.dp),
+                color = Color.White,
+                fontSize = 20.sp
+            )
         }
     }
 }
 
+
+@Preview
+@Composable
+fun ScrollableItemPreview() {
+    val columnScrollState = rememberScrollState()
+    ScrollableItem(
+        columnScrollState = columnScrollState,
+        author = "balls",
+        image = R.drawable.picture1
+    )
+}
 
 const val someRandomText =
     "American rapper and singer Post Malone will have a Bangkok debut concert as part of his \"If Y'All Weren't Here, I'd Be Crying\" world tour, at Impact Challenger Hall, Muang Thong Thani, Nonthaburi province, on Sept 14 at 8.30pm.\n" +

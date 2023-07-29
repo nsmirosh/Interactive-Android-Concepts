36613c53d8c39673f17f3987a3ee8e958baa04dc
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-07-29 23:37:03 +0700

    Bitmaps are being generated from the pictures and displayed in a column

--- app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
@@ -1,11 +1,17 @@
 package nick.mirosh.androidsamples.ui.parallax
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import android.util.TypedValue
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,12 +22,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,54 +47,36 @@ private const val TAG = "ParallaxScreen"
 
 @Composable
 fun ParallaxScreen() {
+//    ScrollableCard()
+    GetEverythingByPixelsPreparation()
     ScrollableCard()
 }
 
 @Composable
 fun ScrollableCard() {
     val columnScrollState = rememberScrollState()
+    GetEverythingByPixelsPreparation()
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(columnScrollState),
     ) {
-
-        ScrollableItem(
+        ScrollableItem2(
             columnScrollState,
             "Massimiliano Morosinotto",
             R.drawable.picture1
         )
-        ScrollableItem(
+        ScrollableItem2(
             columnScrollState,
             "Massimiliano Morosinotto",
             R.drawable.picture1
         )
-        ScrollableItem(
+        ScrollableItem2(
             columnScrollState,
             "Massimiliano Morosinotto",
             R.drawable.picture1
         )
-        ScrollableItem(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
-        )
-        ScrollableItem(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
-        )
-        ScrollableItem(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
-        )
-        ScrollableItem(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
-        )
-        ScrollableItem(
+        ScrollableItem2(
             columnScrollState,
             "Massimiliano Morosinotto",
             R.drawable.picture1
@@ -92,6 +88,8 @@ fun ScrollableCard() {
 @Composable
 fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
     var prevScrollValue by remember { mutableIntStateOf(0) }
+    val scrollState = rememberScrollState()
+    Spacer(modifier = Modifier.height(16.dp))
     Card(
         modifier = Modifier.height(200.dp)
     ) {
@@ -104,6 +102,7 @@ fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
                 modifier = Modifier
                     .background(Color.Yellow)
                     .verticalScroll(columnScrollState)
+                    .gesturesDisabled()
                     .graphicsLayer {
                         val yMovement =
                             columnScrollState.value - prevScrollValue
@@ -115,7 +114,9 @@ fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
             )
             Text(
                 text = author,
-                modifier = Modifier.align(Alignment.BottomStart).padding(16.dp),
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(16.dp),
                 color = Color.White,
                 fontSize = 20.sp
             )
@@ -123,32 +124,91 @@ fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
     }
 }
 
+@Composable
+fun ScrollableItem2(
+    columnScrollState: ScrollState,
+    author: String,
+    image: Int
+) {
+    Spacer(modifier = Modifier.height(16.dp))
+    Card(
+        modifier = Modifier.height(200.dp)
+    ) {
+        GetEverythingByPixels()
+    }
+}
+
+fun Modifier.gesturesDisabled(disabled: Boolean = true) =
+    if (disabled) {
+        pointerInput(Unit) {
+            awaitPointerEventScope {
+                // we should wait for all new pointer events
+                while (true) {
+                    awaitPointerEvent(pass = PointerEventPass.Initial)
+                        .changes
+                        .forEach(PointerInputChange::consume)
+                }
+            }
+        }
+    }
+    else {
+        this
+    }
 
 @Preview
 @Composable
 fun ScrollableItemPreview() {
     val columnScrollState = rememberScrollState()
-    ScrollableItem(
+    ScrollableItem2(
         columnScrollState = columnScrollState,
         author = "balls",
         image = R.drawable.picture1
     )
 }
 
-const val someRandomText =
-    "American rapper and singer Post Malone will have a Bangkok debut concert as part of his \"If Y'All Weren't Here, I'd Be Crying\" world tour, at Impact Challenger Hall, Muang Thong Thani, Nonthaburi province, on Sept 14 at 8.30pm.\n" +
-            "\n" +
-            "The eight-times diamond-certified global superstar just finished his highly successful \"Twelve Carat Tour\" to promote his fourth album Twelve Carat Toothache (2022) this May before he kicked off a new tour in North America early this month, leading up to the release of his fifth album, Austin, on Friday.\n" +
-            "\n" +
-            "He will stop by seven major cities on his Asian leg in September, starting with a gig in Bangkok before heading to perform in Singapore, Manila, Taipei, Seoul, Hong Kong and Tokyo.\n" +
-            "\n" +
-            "Born Austin Richard Post in 1995, Malone began his music career when he was 16 and gained recognition with his 2015 debut single White Iverson, which peaked at number 14 on the US Billboard Hot 100.\n" +
-            "\n" +
-            "He released his debut album Stoney in 2016. It set the record for most weeks on the US Billboard Top R&B/Hip-Hop Albums chart. His second album Beerbongs & Bentleys (2018) debuted at No.1 on the Billboard 200 and set several streaming records. It was nominated for Album of the Year at the 61st Grammy Awards.\n" +
-            "\n" +
-            "\n" +
-            "(Photo courtesy of Live Nation Tero)\n" +
-            "\n" +
-            "His third album Hollywood's Bleeding (2019) also debuted at No.1 on the Billboard 200. It included Sunflower, the lead single to the soundtrack for the film Spider-Man: Into The Spider-Verse (2018). It was nominated for Album of the Year at the 2021 Grammy Awards.\n" +
-            "\n" +
-            "For the upcoming show, Thai fans can expect to enjoy his signature exhilarating performance with songs from his new album as well as fan-favourite hits such as Better Now, I Fall Apart and Circles."
+private var originalBitmap: Bitmap? = null
+
+@Composable
+fun GetEverythingByPixelsPreparation() {
+    val resources = LocalContext.current.resources
+    val opts = BitmapFactory.Options().apply {
+        inScaled =
+            false  // ensure the bitmap is not scaled based on device density
+    }
+    val inputStream = resources.openRawResource(R.raw.picture1)
+    originalBitmap = BitmapFactory.decodeResourceStream(
+        resources,
+        TypedValue(),
+        inputStream,
+        null,
+        opts
+    )
+
+    Log.d(TAG, " GetEverythingByPixelsPreparation: ${originalBitmap?.width}")
+}
+
+@Composable
+fun GetEverythingByPixels(modifier: Modifier = Modifier, scrollBy: Int = 0) {
+    var launch by remember { mutableStateOf(false) }
+    var y by remember { mutableIntStateOf(50) }
+    //convert 200dp to px
+    val pixelValue = with(LocalDensity.current) { 200.dp.roundToPx() }
+
+   //get the amount of pixels that the image column was scrolled
+   //apply that same scroll to the bitmap
+
+    Canvas(
+        modifier = modifier
+            .aspectRatio(originalBitmap!!.width.toFloat() / y)
+    ) {
+        drawIntoCanvas { canvas ->
+            val width = originalBitmap!!.width
+            Log.d(TAG, "GetEverythingByPixels: $width")
+            val newBitmap =
+                Bitmap.createBitmap(originalBitmap!!, 0, 0, width, pixelValue)
+            canvas.nativeCanvas.drawBitmap(newBitmap, 0f, 0f, null)
+        }
+    }
+}
+
+

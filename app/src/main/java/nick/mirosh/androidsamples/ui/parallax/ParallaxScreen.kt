0c9ee2bbecddf0bc1fa52bad6d7a37aaf54a4dc0
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-07-30 14:43:03 +0700

    Implemented an inverted card

--- app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/parallax/ParallaxScreen.kt
@@ -1,140 +1,180 @@
 package nick.mirosh.androidsamples.ui.parallax
 
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Log
 import android.util.TypedValue
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import nick.mirosh.androidsamples.R
 
 
 private const val TAG = "ParallaxScreen"
 
+
+// [ ] - Make the picture scroll the whole height
+// [ ] - Implement picture scrolling in the opposite direction
 @Composable
 fun ParallaxScreen() {
-//    ScrollableCard()
-    GetEverythingByPixelsPreparation()
-    ScrollableCard()
+    var bitmap by remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+    val resources = LocalContext.current.resources
+
+    LaunchedEffect(Unit) {
+        bitmap = decodeBitmap(resources)
+    }
+    bitmap?.let { ScrollableColumn(it) }
 }
 
 @Composable
-fun ScrollableCard() {
+fun ScrollableColumn(bitmap: Bitmap) {
     val columnScrollState = rememberScrollState()
-    GetEverythingByPixelsPreparation()
+    val cardHeight = with(LocalDensity.current) { 200.dp.roundToPx() }
+    var prevScrollValue by remember { mutableIntStateOf(0) }
+//    val yMovement =
+//        columnScrollState.value - prevScrollValue
+    val yMovement =
+        columnScrollState.value
+    prevScrollValue = columnScrollState.value
+    Log.d(
+        TAG,
+        "ScrollableColumn: yMovement $yMovement, prevScrollValue $prevScrollValue"
+    )
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .verticalScroll(columnScrollState),
     ) {
-        ScrollableItem2(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
+        RenderingCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
         )
-        ScrollableItem2(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
+        InvertedCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
         )
-        ScrollableItem2(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
+        RenderingCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
         )
-        ScrollableItem2(
-            columnScrollState,
-            "Massimiliano Morosinotto",
-            R.drawable.picture1
+        InvertedCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
+        )
+        RenderingCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
+        )
+        RenderingCard(
+            originalBitmap = bitmap,
+            cardHeight = cardHeight,
+            totalColumnScrollFromTop = yMovement
         )
     }
 }
 
+fun decodeBitmap(resources: Resources): Bitmap? {
+//    val resources = LocalContext.current.resources
+    val opts = BitmapFactory.Options().apply {
+        inScaled =
+            false  // ensure the bitmap is not scaled based on device density
+    }
+    val inputStream = resources.openRawResource(R.raw.picture1)
+    return BitmapFactory.decodeResourceStream(
+        resources,
+        TypedValue(),
+        inputStream,
+        null,
+        opts
+    )
+}
 
 @Composable
-fun ScrollableItem(columnScrollState: ScrollState, author: String, image: Int) {
-    var prevScrollValue by remember { mutableIntStateOf(0) }
-    val scrollState = rememberScrollState()
-    Spacer(modifier = Modifier.height(16.dp))
+fun RenderingCard(
+    modifier: Modifier = Modifier,
+    originalBitmap: Bitmap,
+    cardHeight: Int,
+    totalColumnScrollFromTop: Int = 0
+) {
     Card(
         modifier = Modifier.height(200.dp)
     ) {
-        Box(
-            modifier = Modifier
+        Canvas(
+            modifier = Modifier.fillMaxSize()
         ) {
-            Image(
-                painter = painterResource(id = image),
-                contentDescription = "Your Image",
-                modifier = Modifier
-                    .background(Color.Yellow)
-                    .verticalScroll(columnScrollState)
-                    .gesturesDisabled()
-                    .graphicsLayer {
-                        val yMovement =
-                            columnScrollState.value - prevScrollValue
-                        prevScrollValue = columnScrollState.value
-                        val adjusted = 0.5f * yMovement.toFloat()
-                        Log.d(TAG, "ScrollableCard: $adjusted")
-                        translationY = adjusted
-                    },
-            )
-            Text(
-                text = author,
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(16.dp),
-                color = Color.White,
-                fontSize = 20.sp
-            )
+            drawIntoCanvas { canvas ->
+                val width = originalBitmap.width
+                Log.d(TAG, "GetEverythingByPixels: $width")
+                val newBitmap =
+                    Bitmap.createBitmap(
+                        originalBitmap,
+                        0,
+                        (totalColumnScrollFromTop * 2) / 10,
+                        width,
+                        cardHeight
+                    )
+                canvas.nativeCanvas.drawBitmap(newBitmap, 0f, 0f, null)
+            }
         }
     }
 }
 
 @Composable
-fun ScrollableItem2(
-    columnScrollState: ScrollState,
-    author: String,
-    image: Int
+fun InvertedCard(
+    modifier: Modifier = Modifier,
+    originalBitmap: Bitmap,
+    cardHeight: Int,
+    totalColumnScrollFromTop: Int = 0
 ) {
-    Spacer(modifier = Modifier.height(16.dp))
     Card(
         modifier = Modifier.height(200.dp)
     ) {
-        GetEverythingByPixels()
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            drawIntoCanvas { canvas ->
+                val width = originalBitmap.width
+                Log.d(TAG, "GetEverythingByPixels: $width")
+                val newBitmap =
+                    Bitmap.createBitmap(
+                        originalBitmap,
+                        0,
+                        1920 - (totalColumnScrollFromTop * 2) / 10,
+                        width,
+                        cardHeight
+                    )
+                canvas.nativeCanvas.drawBitmap(newBitmap, 0f, 0f, null)
+            }
+        }
     }
 }
 
@@ -154,61 +194,4 @@ fun Modifier.gesturesDisabled(disabled: Boolean = true) =
     else {
         this
     }
-
-@Preview
-@Composable
-fun ScrollableItemPreview() {
-    val columnScrollState = rememberScrollState()
-    ScrollableItem2(
-        columnScrollState = columnScrollState,
-        author = "balls",
-        image = R.drawable.picture1
-    )
-}
-
-private var originalBitmap: Bitmap? = null
-
-@Composable
-fun GetEverythingByPixelsPreparation() {
-    val resources = LocalContext.current.resources
-    val opts = BitmapFactory.Options().apply {
-        inScaled =
-            false  // ensure the bitmap is not scaled based on device density
-    }
-    val inputStream = resources.openRawResource(R.raw.picture1)
-    originalBitmap = BitmapFactory.decodeResourceStream(
-        resources,
-        TypedValue(),
-        inputStream,
-        null,
-        opts
-    )
-
-    Log.d(TAG, " GetEverythingByPixelsPreparation: ${originalBitmap?.width}")
-}
-
-@Composable
-fun GetEverythingByPixels(modifier: Modifier = Modifier, scrollBy: Int = 0) {
-    var launch by remember { mutableStateOf(false) }
-    var y by remember { mutableIntStateOf(50) }
-    //convert 200dp to px
-    val pixelValue = with(LocalDensity.current) { 200.dp.roundToPx() }
-
-   //get the amount of pixels that the image column was scrolled
-   //apply that same scroll to the bitmap
-
-    Canvas(
-        modifier = modifier
-            .aspectRatio(originalBitmap!!.width.toFloat() / y)
-    ) {
-        drawIntoCanvas { canvas ->
-            val width = originalBitmap!!.width
-            Log.d(TAG, "GetEverythingByPixels: $width")
-            val newBitmap =
-                Bitmap.createBitmap(originalBitmap!!, 0, 0, width, pixelValue)
-            canvas.nativeCanvas.drawBitmap(newBitmap, 0f, 0f, null)
-        }
-    }
-}
-
 

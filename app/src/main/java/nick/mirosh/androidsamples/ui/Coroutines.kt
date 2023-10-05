79799d149b82e70911e364ac117c0008aaf4cd02
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-10-05 19:00:23 +0700

    Finished with rememberCoroutineScope example

--- app/src/main/java/nick/mirosh/androidsamples/ui/Coroutines.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/Coroutines.kt
@@ -1,18 +1,25 @@
 package nick.mirosh.androidsamples.ui
 
+
 import android.util.Log
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -22,9 +29,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -32,76 +42,183 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
 
-
 val logTag = "Coroutines"
 
-
-enum class UIState {
-    REGULAR, REMEMBER, CANCEL
+@Composable
+fun CancelledIcon(
+    modifier: Modifier = Modifier,
+    color: Color = Color.Red,
+    contentDescription: String? = null
+) {
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = contentDescription,
+        modifier = modifier.size(48.dp),
+        tint = color
+    )
 }
 
 @Composable
-fun Coroutines(
+fun SuccessIcon(
+    modifier: Modifier = Modifier,
+    color: Color = Color.Green,
+    contentDescription: String? = null
 ) {
+    Icon(
+        imageVector = Icons.Default.Check,
+        contentDescription = contentDescription,
+        modifier = modifier.size(48.dp),
+        tint = color
+    )
+}
 
+@Composable
+fun Coroutines() {
     var onCancelPressed by remember {
         mutableStateOf(false)
     }
-
+    var success by remember {
+        mutableStateOf(false)
+    }
 
     var rememberCoroutineScope by remember { mutableStateOf(false) }
 
-
-    Column {
-
-        if (rememberCoroutineScope) {
-            RememberCoroutineScopeComposable(
-                onCancelPressed = {
-                    onCancelPressed = !onCancelPressed
-                }
+    if (success ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             )
-        }
-        else {
-            RegularCoroutineScopeComposable() {
+            {
+                SuccessIcon()
+                Text(
+                    fontSize = 24.sp,
+                    text = "Success!"
+                )
+            }
 
+            Button(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    success = false
+                    onCancelPressed = false
+                }
+            ) {
+                Text(
+                    text = "Try again"
+                )
             }
         }
-
-        Row {
-            Checkbox(checked = rememberCoroutineScope, onCheckedChange = {
-                rememberCoroutineScope = !rememberCoroutineScope
-            })
-            Text(
-                text = "with remember coroutine scope"
+    }
+    else if (onCancelPressed) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             )
+            {
+                CancelledIcon()
+                Text(
+                    fontSize = 24.sp,
+                    text = "Cancelled!"
+                )
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    success = false
+                    onCancelPressed = false
+                }
+            ) {
+                Text(
+                    text = "Try again"
+                )
+            }
+        }
+    }
+    else {
+        Column {
+            if (rememberCoroutineScope) {
+                RememberCoroutineScopeComposable(
+                    onCancelPressed = {
+                        onCancelPressed = !onCancelPressed
+                    },
+                    onFinished = {
+                        success = true
+                    }
+                )
+            }
+            else {
+                RegularCoroutineScopeComposable(
+                    onCancelPressed = {
+                        onCancelPressed = !onCancelPressed
+                    },
+                    onFinished = {
+                        success = true
+                    }
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(checked = rememberCoroutineScope, onCheckedChange = {
+                    rememberCoroutineScope = !rememberCoroutineScope
+                })
+                Text(
+                    fontSize = 16.sp,
+                    text = "use rememberCoroutineScope"
+                )
+            }
         }
     }
 
 }
+
 
 @Composable
 fun RememberCoroutineScopeComposable(
     onCancelPressed: () -> Unit,
+    onFinished: () -> Unit = {}
 ) {
     val rememberCoroutineScope = rememberCoroutineScope()
     var progress by remember { mutableIntStateOf(0) }
     Column {
-        Row {
-            Button(onClick = {
-                rememberCoroutineScope.launch {
-                    Log.d(logTag, "RememberCoroutineScopeComposable: coroutine started")
-                    runCounter {
-                        progress = it
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = {
+                    rememberCoroutineScope.launch {
+                        runCounter {
+                            progress = it
+                        }
+                        onFinished()
                     }
-                    Log.d(logTag, "RememberCoroutineScopeComposable: coroutine finished")
-                }
-            }) {
+                }) {
                 Text(
                     text = "Start"
                 )
             }
-
-            Button(onClick = {
+            Button(
+                modifier = Modifier.padding(start = 16.dp),
+                onClick = {
                 onCancelPressed()
             }) {
                 Text(
@@ -116,27 +233,36 @@ fun RememberCoroutineScopeComposable(
 @Composable
 fun RegularCoroutineScopeComposable(
     onCancelPressed: () -> Unit,
+    onFinished: () -> Unit = {}
 ) {
-
     var progress by remember { mutableIntStateOf(0) }
     Column {
-        Row {
-            Button(onClick = {
-                CoroutineScope(Dispatchers.Main).launch  {
-                    runCounter {
-                        progress = it
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                modifier = Modifier.padding(start = 16.dp),
+                onClick = {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        runCounter {
+                            progress = it
+                        }
+                        onFinished()
                     }
-                }
 
-            }) {
+                }) {
                 Text(
                     text = "Start"
                 )
             }
 
-            Button(onClick = {
-                onCancelPressed()
-            }) {
+            Button(
+
+                modifier = Modifier.padding(start = 16.dp),
+                onClick = {
+                    onCancelPressed()
+                }) {
                 Text(
                     text = "Cancel"
                 )
@@ -146,46 +272,18 @@ fun RegularCoroutineScopeComposable(
     }
 }
 
-@Composable
-fun RememberCoroutineScopeComposable() {
-    val scope = rememberCoroutineScope()
-    Button(onClick = {
-        CoroutineScope(Dispatchers.Main).launch {
-            Log.d(logTag, "RememberCoroutineScopeComposable: coroutine started")
-            delay(5000)
-            Log.d(logTag, "RememberCoroutineScopeComposable: coroutine finished")
-
-        }
-//        scope.launch {
-//            //This scope should cancel when the composition changes
-//            //Because rememberCoroutineScope() only lives as long as
-//            //the composable inside which it is called lives
-//
-//            Log.d(logTag, "RememberCoroutineScopeComposable: coroutine started")
-//            delay(5000)
-//            Log.d(logTag, "RememberCoroutineScopeComposable: coroutine finished")
-//
-//        }
-    }) {
-        Text(
-            text = "Click me"
-        )
-    }
-
-}
-
 
 suspend fun runCounter(
     onCounterUpdate: (Int) -> Unit
 ) {
     var counter = 0
     while (counter < 100) {
-        counter += 10
-
         delay(500)
+        counter += 10
         onCounterUpdate(counter)
     }
 }
+
 @Composable
 fun ProgressWithCancel(progress: Int) {
     val progressAnimDuration = 300

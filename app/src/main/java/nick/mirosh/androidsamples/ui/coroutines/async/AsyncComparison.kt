8f767bbca4977e25f926a57737b1ced83b236bb0
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-11-15 18:19:49 +0700

    Finished with the example of the coroutine scope completely

--- app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/async/AsyncComparison.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/async/AsyncComparison.kt
@@ -1,17 +1,28 @@
 package nick.mirosh.androidsamples.ui.coroutines.async
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -21,37 +32,106 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 fun AsyncComparisonScreen(
     viewModel: AsyncComparisonViewModel = hiltViewModel()
 ) {
-    val state by viewModel.uiState.collectAsStateWithLifecycle()
-    with (state) {
-        when (state) {
-            is AsyncComparisonUIState.DeferredUpdate -> {
-                ProgressBar(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                        .height(32.dp)
-                        .clip(RoundedCornerShape(16.dp)),
-                    progress = (progress * 100).toInt()
-                )
+    val deferred1Updates by viewModel.deferred1Flow.collectAsStateWithLifecycle()
+    val deferred2Updates by viewModel.deferred2Flow.collectAsStateWithLifecycle()
+    val job1FlowUpdates by viewModel.job1flow.collectAsStateWithLifecycle()
+    val job2FlowUpdates by viewModel.job2flow.collectAsStateWithLifecycle()
+    var restart by remember {
+        mutableStateOf(false)
+    }
+    key(restart) {
+        Column {
+            Button(
+                onClick = {
+                    viewModel.launchAsyncs()
+                }
+            ) {
+                Text("Launch asyncs")
+            }
+            Button(
+                onClick = {
+                    viewModel.launchCoroutines()
+                }
+            ) {
+                Text("Launch coroutines")
+            }
+
+            ProgressBarWithCancel(
+                progress = deferred1Updates,
+                label = "Deferred 1",
+                onCancelClick = {
+                    viewModel.cancelAsync1()
+                }
+            )
+            ProgressBarWithCancel(
+                progress = deferred2Updates,
+                label = "Deferred 2",
+                onCancelClick = {
+                    viewModel.cancelAsync2()
+                }
+            )
+            ProgressBarWithCancel(
+                progress = job1FlowUpdates,
+                label = "Coroutine 1",
+                onCancelClick = {
+                    viewModel.cancelCoroutine1()
+                }
+            )
+            ProgressBarWithCancel(
+                progress = job2FlowUpdates,
+                label = "Coroutine 2",
+                onCancelClick = {
+                    viewModel.cancelCoroutine2()
+                }
+            )
+            Button(onClick = {
+                viewModel.clear()
+                restart = !restart
+            }) {
+                Text("Restart")
             }
         }
     }
+}
 
-    Column {
-        Button(
-            onClick = {
-                viewModel.launchAsyncs()
-            }
-        ) {
-            Text("Launch asyncs")
-        }
+
+@Composable
+fun ProgressBarWithCancel(
+    modifier: Modifier = Modifier,
+    progress: Float,
+    label: String,
+    onCancelClick: () -> Unit,
+) {
+
+    var cancelled by remember {
+        mutableStateOf(false)
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    Text(label)
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        ProgressBar(
+            modifier = modifier
+                .weight(2f / 3f)
+                .padding(horizontal = 16.dp),
+            progress = progress
+        )
 
         Button(
+            colors = if (cancelled) ButtonDefaults.buttonColors(
+                backgroundColor = Color.Red,
+                contentColor = Color.White
+            )
+            else ButtonDefaults.buttonColors(),
+            modifier = Modifier
+                .weight(1f / 3f)
+                .padding(horizontal = 16.dp),
             onClick = {
-                viewModel.launchCoroutines()
-            }
-        ) {
-            Text("Launch coroutines")
+                cancelled = true
+                onCancelClick()
+            }) {
+            Text(if (cancelled) "Cancelled" else "Cancel")
         }
     }
 }
@@ -59,15 +139,23 @@ fun AsyncComparisonScreen(
 
 @Composable
 fun ProgressBar(
-    modifier: Modifier,
-    progress: Int,
+    modifier: Modifier = Modifier,
+    progress: Float,
 ) {
 
+    val progressAnimDuration = 300
+    val progressAnimation by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(
+            durationMillis = progressAnimDuration,
+            easing = FastOutSlowInEasing
+        ),
+        label = ""
+    )
     LinearProgressIndicator(
-        progress = progress / 100f,
-        modifier = Modifier
+        progress = progressAnimation,
+        modifier = modifier
             .padding(horizontal = 16.dp)
-            .fillMaxWidth()
             .height(32.dp)
             .clip(RoundedCornerShape(16.dp))
     )
--- app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/async/AsyncComparisonViewModel.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/async/AsyncComparisonViewModel.kt
@@ -3,6 +3,8 @@ package nick.mirosh.androidsamples.ui.coroutines.async
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
@@ -13,45 +15,82 @@ import kotlin.system.measureTimeMillis
 
 class AsyncComparisonViewModel : ViewModel() {
 
-    private val _uiState = MutableStateFlow(AsyncComparisonUIState.DeferredUpdate(0f, 0))
-    val uiState = _uiState.asStateFlow()
+    private val _deferred1Flow = MutableStateFlow(0f)
+    val deferred1Flow = _deferred1Flow.asStateFlow()
+
+    private val _deferred2Flow = MutableStateFlow(0f)
+    val deferred2Flow = _deferred2Flow.asStateFlow()
+
+    private val _job1flow = MutableStateFlow(0f)
+    val job1flow = _job1flow.asStateFlow()
+
+    private val _job2flow = MutableStateFlow(0f)
+    val job2flow = _job2flow.asStateFlow()
+
+    private var deferred1: Deferred<Unit>? = null
+    private var deferred2: Deferred<Unit>? = null
+    private var job1: Job? = null
+    private var job2: Job? = null
 
     fun launchAsyncs() {
         viewModelScope.launch {
             val timeSpent = measureTimeMillis {
-                val deferred = async {
-                    delay(100)
-                    _uiState.value = AsyncComparisonUIState.DeferredUpdate(0.1f, 1)
+                deferred1 = async {
+                    runUpdatesIn(_deferred1Flow)
                 }
-                val deffered2 = async {
-                    delay(100)
-                    _uiState.value = AsyncComparisonUIState.DeferredUpdate(0.1f, 2)
+                deferred2 = async {
+                    runUpdatesIn(_deferred2Flow)
+
                 }
-                awaitAll(deferred, deffered2)
+                awaitAll(deferred1!!, deferred2!!)
             }
             Log.d("AsyncComparison", "asyncs finished in $timeSpent")
         }
     }
 
+
+    fun cancelAsync1() {
+        deferred1?.cancel()
+    }
+
+    fun cancelAsync2() {
+        deferred2?.cancel()
+    }
+
+    fun cancelCoroutine1() {
+        job1?.cancel()
+    }
+
+    fun cancelCoroutine2() {
+        job2?.cancel()
+    }
+
+    fun clear() {
+        job1?.cancel()
+        job2?.cancel()
+        deferred1?.cancel()
+        deferred2?.cancel()
+        _deferred1Flow.value = 0f
+        _deferred2Flow.value = 0f
+        _job1flow.value = 0f
+        _job2flow.value = 0f
+    }
+
     fun launchCoroutines() {
-        viewModelScope.launch {
-            val timeSpent = measureTimeMillis {
-                val job = viewModelScope.launch {
-                    delay(1000)
-                }
-                val job2 = viewModelScope.launch {
-                    delay(1000)
-                }
-                job.join()
-                job2.join()
-            }
-            Log.d("AsyncComparison", "coroutines finished in $timeSpent")
+        job1 = viewModelScope.launch {
+            runUpdatesIn(_job1flow)
+        }
+        job2 = viewModelScope.launch {
+            runUpdatesIn(_job2flow)
         }
     }
-}
 
-sealed class AsyncComparisonUIState {
-    data object Initial : AsyncComparisonUIState()
-    data class DeferredUpdate(val progress: Float, val no: Int) : AsyncComparisonUIState()
-    data class CoroutineUpdate(val progress: Float, val no: Int) : AsyncComparisonUIState()
+    private suspend fun runUpdatesIn(flow: MutableStateFlow<Float>) {
+        var counter = 0
+        while (counter <= 100) {
+            delay(500)
+            flow.value = counter / 100f
+            counter += 10
+        }
+    }
 }

3c0cd780249da28f5f436e3660e48aa71a9beec6
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-11-19 23:43:19 +0700

    Finished with the coroutine scope exercise for the most part

--- app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/coroutine_scope/CoroutineScopeViewModel.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/coroutine_scope/CoroutineScopeViewModel.kt
@@ -1,5 +1,6 @@
 package nick.mirosh.androidsamples.ui.coroutines.coroutine_scope
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.coroutineScope
@@ -7,47 +8,55 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import nick.mirosh.androidsamples.ui.coroutines.runUpdatesIn
 
-const val updateSpeed = 300L
 
 class CoroutineScopeViewModel : ViewModel() {
 
-
-    //TODO refactor to use 1 flow
-    private val _task1Flow = MutableStateFlow(0f)
+    private val _task1Flow = MutableStateFlow(Pair(1f, "Task 1 : 2.0s"))
     val task1Flow = _task1Flow.asStateFlow()
 
-    private val _task2Flow = MutableStateFlow(0f)
+    private val _task2Flow = MutableStateFlow(Pair(1f, "Task 2 : 5.0s"))
     val task2Flow = _task2Flow.asStateFlow()
 
-    private val _task3Flow = MutableStateFlow(0f)
+    private val _task3Flow = MutableStateFlow(Pair(1f, "Task 3 : 1.0s"))
     val task3Flow = _task3Flow.asStateFlow()
 
-    private val _task4Flow = MutableStateFlow(0f)
+    private val _task4Flow = MutableStateFlow(Pair(1f, "Task 4: 1.0s"))
     val task4Flow = _task4Flow.asStateFlow()
 
     fun onCoroutineScopeClicked() {
         viewModelScope.launch {
             launch {
-                delay(200L)
+                sendTimeUpdatesInto(_task1Flow, 3000L, 1)
                 println("Task 1")
-                runUpdatesIn(_task1Flow, updateSpeed)
             }
 
             coroutineScope {
                 launch {
-                    delay(500L)
+                    sendTimeUpdatesInto(_task2Flow, 4000L, 2)
                     println("Task 2")
-                    runUpdatesIn(_task2Flow, updateSpeed)
                 }
-                delay(100L)
+                sendTimeUpdatesInto(_task3Flow, 2000L, 3)
                 println("Task 3")
-                runUpdatesIn(_task3Flow, updateSpeed)
             }
 
+            sendTimeUpdatesInto(_task4Flow, 2000L, 4)
             println("Task 4")
-            runUpdatesIn(_task4Flow, updateSpeed)
+        }
+    }
+
+    private suspend fun sendTimeUpdatesInto(
+        flow: MutableStateFlow<Pair<Float, String>>,
+        delay: Long,
+        taskNo: Int = 0
+    ) {
+        for (i in (delay / 100).toInt() downTo 0) {
+            Log.d(
+                "CoroutineScopeViewModel",
+                "sendTimeUpdatesInto: $${i.toFloat() / (delay / 100)} i = $i"
+            )
+            flow.value = Pair(i.toFloat() / (delay / 100), "Task $taskNo : ${i / 10},${i % 10}s")
+            delay(100)
         }
     }
 }

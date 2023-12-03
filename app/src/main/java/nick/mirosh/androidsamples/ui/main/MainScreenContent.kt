88beb80ca76cec0252db3915f86748f882baf3a1
Nick Miroshnychenko <mykola.miroshnychenko@7peakssoftware.com>
2023-12-03 19:13:53 +0700

    Fixed the UI a bit

--- app/src/main/java/nick/mirosh/androidsamples/ui/main/MainScreenContent.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/main/MainScreenContent.kt
@@ -32,6 +32,22 @@ fun MainScreenContent(
 
     Column(modifier = Modifier.verticalScroll(scrollState)) {
         Text(
+            text = "Coroutines",
+            modifier = Modifier
+                .clickable {
+                    onCoroutinesClicked?.invoke()
+                }
+                .padding(24.dp)
+        )
+        Text(
+            text = "Launched Effect",
+            modifier = Modifier
+                .clickable {
+                    onSideEffectsClicked?.invoke()
+                }
+                .padding(24.dp)
+        )
+        Text(
             text = "Simple List",
             modifier = Modifier
                 .clickable {
@@ -72,14 +88,6 @@ fun MainScreenContent(
                 .padding(24.dp)
         )
         Text(
-            text = "Launched Effect",
-            modifier = Modifier
-                .clickable {
-                    onSideEffectsClicked?.invoke()
-                }
-                .padding(24.dp)
-        )
-        Text(
             text = "Disposable Effect",
             modifier = Modifier
                 .clickable {
@@ -100,14 +108,6 @@ fun MainScreenContent(
             modifier = Modifier
                 .clickable {
                     onParallaxScreenClicked?.invoke()
-                }
-                .padding(24.dp)
-        )
-        Text(
-            text = "Coroutines",
-            modifier = Modifier
-                .clickable {
-                    onCoroutinesClicked?.invoke()
                 }
                 .padding(24.dp)
         )

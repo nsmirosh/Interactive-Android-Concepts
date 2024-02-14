66ddf51b463a40b718f06e79ec3a2b74fec0207c
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2024-02-15 02:26:23 +0700

    Enhanced the DeadLock a bit more

--- app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/deadlock/DeadLockViewModel.kt
+++ app/src/main/java/nick/mirosh/androidsamples/ui/coroutines/deadlock/DeadLockViewModel.kt
@@ -5,24 +5,36 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 
 private const val TAG = "DeadLockViewModel"
-class DeadLockViewModel: ViewModel() {
+
+class DeadLockViewModel : ViewModel() {
 
     fun runLogicalDeadlock() {
-
         viewModelScope.launch {
-            var deferred2: Deferred<String>? = null
+            try {
+                withTimeout(5000L) {
+                    deadLock()
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Caught exception: ${e.message}")
+            }
+        }
+    }
 
+    private suspend fun deadLock() {
+        coroutineScope {
+            var deferred2: Deferred<String>? = null
             val deferred1 = async {
                 Log.d(TAG, "runDeadlock: awaiting task 2 to complete")
                 val result2 = deferred2?.await()
                 Log.d(TAG, "task 1 completed")
                 "Result of task 1 depends on task 2: $result2"
             }
-
             deferred2 = async {
 
                 Log.d(TAG, "runDeadlock: awaiting task 1 to complete")
@@ -30,13 +42,9 @@ class DeadLockViewModel: ViewModel() {
                 Log.d(TAG, "task 2 completed")
                 "Result of task 2 depends on task 1: $result1"
             }
-            try {
-                Log.d(TAG, "runDeadlock: waiting for tasks to complete")
-                deferred1.await()
-                deferred2.await()
-            } catch (e: Exception) {
-                Log.e(TAG, "Caught exception: ${e.message}")
-            }
+            Log.d(TAG, "runDeadlock: waiting for tasks to complete")
+            deferred1.await()
+            deferred2.await()
         }
     }
 

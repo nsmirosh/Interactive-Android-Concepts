8756270e3ef906b18b7cf1fabd1155215c15626b
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2024-11-03 19:43:33 +0700

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "nick.mirosh.androidsamples"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "nick.mirosh.androidsamples"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -67,51 +67,55 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    //gson converter factory dependency
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    //okhttp dependency
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    //logging interceptor dependency
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    val retrofit = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    //gson converter factory dependency
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("androidx.navigation:navigation-compose:2.8.3")
 
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    val hilt = "2.52"
+    implementation("com.google.dagger:hilt-android:$hilt")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material:1.7.5")
 
-    implementation("com.google.dagger:hilt-android:2.49")
-    kapt("com.google.dagger:hilt-android-compiler:2.49")
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.material:material:1.6.5")
 
-   //lottie
+    implementation ("com.airbnb.android:lottie-compose:6.4.1")
 
-    implementation ("com.airbnb.android:lottie-compose:6.2.0")
-
-    //depdendency for coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    val coroutines = "1.9.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutines")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    val lifecycle = "2.8.7"
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.5")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycle")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    val uiTest = "1.7.5"
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$uiTest")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$uiTest")
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -122,7 +126,6 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
     implementation("com.github.nsmirosh:ParallaxColumn:1.0.4")
 }
